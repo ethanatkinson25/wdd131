6f8a45c40f14e9cf87d73b2484a205b2f9dc07e1
ethanatkinson <ethanatkinson11@icloud.com>
2025-09-10 19:24:24 -0600

    Completed JS DOM events activity

--- week02/scripts/bom-js.js
+++ week02/scripts/bom-js.js
@@ -1,10 +1,24 @@
-const input = document.querySelector('#favchap');
 const button = document.querySelector('button');
-const list = document.querySelector('list');
-const li = document.querySelector('li');
-const deleteButton = document.querySelector('button');
+const input = document.querySelector('#favchap');
+document.getElementById("button").innerText = "Enter a chapter";
+const list = document.querySelector('#list');
 
-li.textContent = input.value;
-button.textContent = "X";
-li.append(deleteButton);
-list.append(li);
+
+
+button.addEventListener("click", function(){
+    if (input.value.trim() !== ""){
+        const li = document.querySelector('li');
+        li.textContent = input.value;
+        button.textContent = "X";
+        li.append(deleteButton);
+        list.append(li);
+    }
+})
+const deleteButton = document.querySelector('button');
+deleteButton.addEventListener("click", function(){
+    list.removeChild(li);
+    input.focus();
+    input.value = "";
+    input.focus();
+})
+
