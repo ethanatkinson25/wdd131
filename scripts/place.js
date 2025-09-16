df207938771c6c409e27e5ae692ea2bc2b7d868e
ethanatkinson <ethanatkinson11@icloud.com>
2025-09-16 13:29:42 -0600

    Began Country website project

---
+++ scripts/place.js
@@ -0,0 +1,19 @@
+console.log("Script is running...");
+
+const authorName = "Ethan Atkinson | Utah";
+
+const yearSpan = document.getElementById("currentyear");
+const modifiedP = document.getElementById("lastModified");
+
+const now = new Date();
+const thisYear = now.getFullYear();
+const lastModDate = new Date(document.lastModified);
+
+if (yearSpan) {
+  yearSpan.textContent = `${authorName} | ${thisYear}`;
+}
+
+if (modifiedP) {
+  modifiedP.textContent = `Last Modified: ${lastModDate.toLocaleString()}`;
+}
+
