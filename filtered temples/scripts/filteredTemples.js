f97a8e6d9133b2bc6c6840c8d7910f623dda2b93
ethanatkinson <ethanatkinson11@icloud.com>
2025-09-24 17:35:21 -0600

    Began final project mobile view

---
+++ filtered temples/scripts/filteredTemples.js
@@ -0,0 +1,24 @@
+const authorName = "Ethan Atkinson | Utah";
+
+const yearSpan = document.getElementById("currentyear");
+const modifiedP = document.getElementById("lastModified");
+
+const now = new Date();
+const thisYear = now.getFullYear();
+const lastModDate = new Date(document.lastModified);
+
+if (yearSpan) {
+  yearSpan.textContent = `${authorName} | ${thisYear}`;
+}
+
+if (modifiedP) {
+  modifiedP.textContent = `Last Modified: ${lastModDate.toLocaleString()}`;
+}
+
+const hamburgerButton = document.querySelector("#menu");
+const navigation  = document.querySelector(".navigation");
+
+hamburgerButton.addEventListener("click", () => {
+    navigation.classList.toggle("open");
+    hamburgerButton.classList.toggle("open");
+});
