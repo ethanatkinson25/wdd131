25801f4b639a52be5eda2aa7a82aac743371d90e
ethanatkinson <ethanatkinson11@icloud.com>
2025-10-01 14:12:11 -0600

    Started HTML form activity

---
+++ scripts/form.js
@@ -0,0 +1,46 @@
+const authorName = "Ethan Atkinson | Utah";
+
+const yearSpan = document.getElementById("currentyear");
+const modifiedP = document.getElementById("lastModified");
+
+const now = new Date();
+const thisYear = now.getFullYear();
+const lastModDate = new Date(document.lastModified);
+
+const products = [
+  {
+    id: "fc-1888",
+    name: "flux capacitor",
+    averagerating: 4.5
+  },
+  {
+    id: "fc-2050",
+    name: "power laces",
+    averagerating: 4.7
+  },
+  {
+    id: "fs-1987",
+    name: "time circuits",
+    averagerating: 3.5
+  },
+  {
+    id: "ac-2000",
+    name: "low voltage reactor",
+    averagerating: 3.9
+  },
+  {
+    id: "jj-1969",
+    name: "warp equalizer",
+    averagerating: 5.0
+  }
+];
+
+
+
+if (yearSpan) {
+  yearSpan.textContent = `${authorName} | ${thisYear}`;
+}
+
+if (modifiedP) {
+  modifiedP.textContent = `Last Modified: ${lastModDate.toLocaleString()}`;
+}
